0ec01d7ebcbbc519b1df300f3ad7b8572a9d55d3
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-12 18:53:19 -0700

    [CHORE] render json of facebook callback

--- app/controllers/facebook/sessions_controller.rb
+++ app/controllers/facebook/sessions_controller.rb
@@ -1,9 +1,7 @@
-class Facebook::SessionsController < ApplicationController
-  # GET /auth/facebook/callback
+class Facebook::SessionsController < ApplicationController # GET /auth/facebook/callback
   def create
     begin
       render json: request.env['omniauth.auth']
-      session[:facebook] = request.env['omniauth.auth']
     rescue => e
       render json: 'error'
       session[:facebook] = nil
