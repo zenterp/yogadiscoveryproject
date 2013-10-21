ef3d0d81853c892d61c05e67c17cdda2b8845c84
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-20 21:09:57 -0700

    [FEATURE] Handle * routes with front end router

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Yogadiscoveryproject::Application.routes.draw do
   devise_for :users
 
-  namespace :api do 
-    resources :studios
-  end 
-
   get 'auth/facebook/callback', to: 'facebook/sessions#create'
-  get 'studios/new', to: 'studios#new'
 
-  get '/', to: 'studios#nearby'
+  get '*', to: 'studios#nearby'
 end
